8fc3d464b1ddc2d070757e292095689737c1a5cc
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-06-23 19:50:45 -0700

    Deleted jQuery files, added name outline svg and navbar js file

---
+++ navbar.js
@@ -0,0 +1,9 @@
+fetch('navbar.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.id = 'navbar';
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
