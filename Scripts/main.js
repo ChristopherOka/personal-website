3d262fb74c499575d2c167bcf49c54dfbc02f748
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-06-27 19:01:27 -0700

    added projectile motion testing functions

--- Scripts/main.js
+++ Scripts/main.js
@@ -45,7 +45,63 @@ async function bindRedirects(){
     }
 }
 
+function projectileEqnCoordGenerator(initCoords, initVelocities, t) {
+    const x = initCoords[0];
+    const y = initCoords[1];
+    const vX = initVelocities[0];
+    const vY = initVelocities[1];
+    const g = 9.81; // px/s^2
+    /* 
+    projectile motion equations: x = x0 + vX*t
+                                 y = y0 + vY0*t - 1/2(9.81)*t^2
+                                 vXf = vX
+                                 vYf = vY - g*t
+    */
+// [x,y,vYf]
+    return [x + vX*t, y + vY*t - (g/2)*(t**2), vY - g*t];
+}
+
+async function testProjectile() {
+    const x0 = 10;
+    const y0 = 10;
+    const vX0 = 30;
+    const vY0 = 120;
+    let vYf = vY0;
+    let xf = x0;
+    let yf = y0;
+    const body = document.body;
+    let t = 0;
+   
+    while (t < 20) {
+        const coords = projectileEqnCoordGenerator([x0,y0], [vX0,vY0], t);
+        xf = Math.round(coords[0] * 100)/100;
+        yf = Math.round(coords[1] * 100)/100;
+        vYf = Math.round(coords[2] * 100)/100;
+        body.innerHTML += `<div class='proj' style='left: ${xf}px; bottom: ${yf}px'></div>`;
+        t++;
+    }
+    t = 0;
+    while (t < 20) {
+        const coords = projectileEqnCoordGenerator([xf, yf], [vX0, -vYf], t);
+        x = Math.round(coords[0] * 100)/100;
+        y = Math.round(coords[1] * 100)/100;
+        vy = Math.round(coords[2] * 100)/100;
+        body.innerHTML += `<div class='proj' style='left: ${x}px; bottom: ${y}px'></div>`;
+        t++;
+    }
+}
+
+async function testfn() {
+    const start = Date.now();
+
+    await testProjectile();
+
+    const end = Date.now();
+    console.log(`Execution time: ${end - start} ms`); 
+}
+
 window.onload = () => {
+   testfn();
     handleClick();
     handlePop();
 };
