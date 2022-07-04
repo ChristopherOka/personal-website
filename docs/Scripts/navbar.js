ed1585e6a5efce69318c856fc057925bf441ae5e
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-07-03 20:27:36 -0700

    removed unused files

--- docs/Scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-fetch('./HTML/navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.id = 'navbar';
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
